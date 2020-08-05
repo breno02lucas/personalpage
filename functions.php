4c50f1d55412c44595de8248685a0e3762950e0d
Breno Lucas da Silva Soares <53299626+breno02lucas@users.noreply.github.com>
2020-08-05 02:45:39 -0300

    Pegar o nome do usuário

--- functions.php
+++ functions.php
@@ -3,6 +3,16 @@
     // Requires
     require_once('incs/day.php');
 
+    // Save Visitant Name
+    if(isset($_GET['name']))
+    {
+        $name = $_POST['visitante'];
 
+        session_start();
+        $_SESSION['visitantName'] = $name;
+
+        header("Location:../index.php");
+        exit();
+    }
 
 ?>
